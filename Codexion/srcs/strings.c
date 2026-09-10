6c7f4880e00b0ac7a44eaae416cb03ae0c5b67cb
Luz <estelarodrigoluz@gmail.com>
2026-09-10 17:32:46 +0100

    ft-strcmp now works properly

--- Codexion/srcs/strings.c
+++ Codexion/srcs/strings.c
@@ -19,7 +19,7 @@ int ft_strcmp(const char *s1, const char *s2)
 	i = 0;
 	if (!s1 || !s2)
 		return (1);
-	while(s1[i] == s2[i] && s1[i] && s2[i])
+	while(s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
